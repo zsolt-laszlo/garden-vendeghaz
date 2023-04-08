7b020b804a1643b94416e23a6a7d4c1ca2ceaba4
Laszlo Zsolt <laszlo_zsolt@outlook.com>
2023-04-08 12:45:08 +0300

    0.0.1

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
 //galey script
-document.getElementById('next').onclick = function(){
-  let lists = document.querySelectorAll('.item');
-  document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function(){
-  let lists = document.querySelectorAll('.item');
-  document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
 
-//latvanyosagok kep kiugrik
-
-function zoom(x) {
-  x.style.height = "350px";
-  x.style.width = "350px";
-}
-
-function normal(x) {
-  x.style.height = "250px";
-  x.style.width = "250px";
-}
